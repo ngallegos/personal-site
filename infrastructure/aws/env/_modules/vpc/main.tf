67b3ef6523d6caf8b10b018fc1ef3f624c5806ea
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-05-09 11:49:00 -0600

    GH-3 Fixed the nat gateway id so terraform wouldn't always tell me there were changes to be made

--- infrastructure/aws/env/_modules/vpc/main.tf
+++ infrastructure/aws/env/_modules/vpc/main.tf
@@ -118,7 +118,7 @@ resource "aws_route_table" "private_a" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway_a.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway_a.id
   }
 
   tags = {
@@ -131,7 +131,7 @@ resource "aws_route_table" "private_b" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway_b.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway_b.id
   }
 
   tags = {
