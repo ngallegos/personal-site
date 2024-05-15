c9cd95fcc4a8462cdfa848d1663d60eb179be93b
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-05-15 13:19:09 -0600

    GH-3 Reverted s3 state storage while I rewrite the vpc setup

--- infrastructure/aws/env/prod/main.tf
+++ infrastructure/aws/env/prod/main.tf
@@ -9,20 +9,20 @@ provider "aws" {
   }
 }
 
-terraform {
-  # If you do not want to store the state file in S3, comment out the following and leave it commented out 
-  backend "s3" {}
-  # First init setup if you want to store the state file in S3:
-  # 1. comment out the above block 
-  # 1. run `terraform apply` first to create the s3 bucket
-  # 2. uncomment the following block
-  # 3. create a backend.conf file with the following content:
-  #     region         = "your-aws-region"
-  #     bucket         = "your-terraform-state-bucket"
-  #     key            = "states/app/terraform.tfstate"
-  #     profile        = "<your-aws-cli-profile>"
-  # 4. run `terraform init -migrate-state -backend-config=backend.conf`
-}
+# terraform {
+#   # If you do not want to store the state file in S3, comment out the following and leave it commented out 
+#   backend "s3" {}
+#   # First init setup if you want to store the state file in S3:
+#   # 1. comment out the above block 
+#   # 1. run `terraform apply` first to create the s3 bucket
+#   # 2. uncomment the following block
+#   # 3. create a backend.conf file with the following content:
+#   #     region         = "your-aws-region"
+#   #     bucket         = "your-terraform-state-bucket"
+#   #     key            = "states/app/terraform.tfstate"
+#   #     profile        = "<your-aws-cli-profile>"
+#   # 4. run `terraform init -migrate-state -backend-config=backend.conf`
+# }
 
 module "vpc" {
   source                = "../_modules/vpc"
