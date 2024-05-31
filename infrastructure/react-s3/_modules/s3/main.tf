ede2f955b78b81db014f0b7398fbc0a3762e0cf8
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-05-31 13:05:15 -0600

    GH-4 got s3 configured in terraform

--- infrastructure/react-s3/_modules/s3/main.tf
+++ infrastructure/react-s3/_modules/s3/main.tf
@@ -3,13 +3,35 @@ resource "aws_s3_bucket" "static_site" {
     bucket = "${var.site_name}-personal-site-react-s3-${var.env}"
 }
 
+# make things public so we can host a static site
+resource "aws_s3_bucket_public_access_block" "allow_public_access" {
+    bucket = aws_s3_bucket.static_site.id
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+}
+
+#configure the static site hosting from s3
+resource "aws_s3_bucket_website_configuration" "static_site" {
+    bucket = aws_s3_bucket.static_site.id
+    index_document { 
+        suffix = "index.html"
+    }
+    error_document { 
+        key = "index.html" 
+    }
+}
+
 resource "aws_s3_bucket_policy" "allow_public_access" {
     bucket = aws_s3_bucket.static_site.id
     policy = data.aws_iam_policy_document.static_site_bucket_policy.json
-
+    depends_on = [
+        aws_s3_bucket_website_configuration.static_site
+    ]
 }
 
-#create the static site bucket policy
+# #create the static site bucket policy
 data "aws_iam_policy_document" "static_site_bucket_policy" {
     statement {
         actions = [
--- infrastructure/react-s3/_modules/s3/outputs.tf
+++ infrastructure/react-s3/_modules/s3/outputs.tf
@@ -1,3 +1,11 @@
 output "static_site_bucket" {
   value       = aws_s3_bucket.static_site.id
 }
+
+output "static_site_website_domain" {
+  value       = aws_s3_bucket_website_configuration.static_site.website_domain
+}
+
+output "static_site_website_endpoint" {
+  value       = aws_s3_bucket_website_configuration.static_site.website_endpoint
+}
